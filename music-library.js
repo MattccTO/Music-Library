9b7e2a98c5a6b175f3a14773d66d7e5e98cc7c36
Matt Cook <mcc1985@gmail.com>
2019-03-15 16:46:15 +0000

    Instantiate tracks, playlists, and library

--- music-library.js
+++ music-library.js
@@ -42,9 +42,48 @@ class Playlist {
 }
 
 class Track {
-  constructor(title, rating, length) {
+  constructor(title, artist, rating, length) {
     this.title = title;
+    this.artist = artist;
     this.rating = rating;
     this.length = length;
   }
 }
+
+const creamOnChrome = new Track('Cream on Chrome', 'Ratatat', 4.75, 255);
+const supreme = new Track('Supreme', 'Ratatat', 3.5, 180);
+const wildcat = new Track('Wildcat', 'Ratatat', 4.5, 261);
+const loudPipes = new Track('Loud Pipes', 'Ratatat', 4, 227);
+
+const programmingMusic = new Playlist('Programming Music');
+programmingMusic.addTrack(creamOnChrome);
+programmingMusic.addTrack(supreme);
+programmingMusic.addTrack(wildcat);
+programmingMusic.addTrack(loudPipes);
+
+const virus = new Track('Virus', 'Deltron 3030', 4, 266);
+const turbulence = new Track('Turbulence', 'Deltron 3030', 4.25, 213);
+const zombies = new Track('Zombies', 'Childish Gambino', 4, 281);
+const redbone = new Track('Redbone', 'Childish Gambino', 5, 326);
+
+const chillRap = new Playlist('Chill Rap');
+chillRap.addTrack(virus);
+chillRap.addTrack(turbulence);
+chillRap.addTrack(zombies);
+chillRap.addTrack(redbone);
+
+const fiveHundredChannels = new Track('500 Channels', 'Choking Victim', 4, 160);
+const timeBomb = new Track('Time Bomb', 'Rancid', 4.25, 144);
+const allHisSuits = new Track('All His Suits Are Torn', 'NOFX', 5, 139);
+const minorThreat = new Track('Minor Threat', 'Minor Threat', 5, 87);
+
+const punkRock = new Playlist('Punk Rock');
+punkRock.addTrack(fiveHundredChannels);
+punkRock.addTrack(timeBomb);
+punkRock.addTrack(allHisSuits);
+punkRock.addTrack(minorThreat);
+
+const mattsLibrary = new Library('Matt\'s Library');
+mattsLibrary.addPlaylist(programmingMusic);
+mattsLibrary.addPlaylist(chillRap);
+mattsLibrary.addPlaylist(punkRock);
